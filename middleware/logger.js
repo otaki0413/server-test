9d09f4ba69de9af3a38acf2409b46e1b02b3bf4f
otaki0413 <takida0413@gmail.com>
2023-01-30 00:36:12 +0900

    ✨ 包括的エラーハンドリング練習

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,4 +1,10 @@
+// ログ出力用ミドルウェア
 exports.logMiddleWare = (req, res, next) => {
   console.log(Date.now(), req.method, req.url);
   next();
+};
+
+// エラー用ミドルウェア
+exports.errorMiddleWare = (req, res, next) => {
+  next(new Error("ミドルウェアからのエラー"));
 };
--- server.js
+++ server.js
@@ -5,33 +5,27 @@ const app = express();
 // アプリケーション全体に設定
 app.use(logMiddleWare);
 
-// エラー用ミドルウェア
-const errorMiddleWare = (req, res, next) => {
-  next(new Error("ミドルウェアからのエラー"));
-};
-
 // GET / アクセス時の挙動
 app.get("/", logMiddleWare, (req, res) => {
   res.status(200).send("hello world\n");
 });
 
-// GET /user/:id アクセス時の挙動
-app.get("/user/:id", logMiddleWare, (req, res) => {
-  res.status(200).send(req.params.id);
-});
-
 // エラー用のルート
-app.get("/err", errorMiddleWare, (req, res) => {
-  console.log("errルート");
-  res.status(200).send("errルート");
+// app.get("/err", errorMiddleWare, (req, res) => {
+//   console.log("errルート");
+//   res.status(200).send("errルート");
+// });
+
+app.get("/err", (req, res) => {
+  throw new Error("同期的なエラーです");
 });
 
-// 包括的エラーハンドリング（引数4つ）
+// 包括的エラーハンドリング
 app.use((err, req, res, next) => {
   res.status(500).send("Internal Server Error");
 });
 
-// 3001番でサーバー起動
+// 3002番でサーバー起動
 app.listen(3002, () => {
   console.log("start listening");
 });
